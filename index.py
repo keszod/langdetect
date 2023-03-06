e715daff4984f5be8adfd0a4fa8ac88ff6295adb
keszod <52524513+keszod@users.noreply.github.com>
2023-03-06 08:18:21 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -4,58 +4,22 @@ from time import sleep
 from langdetect import detect
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup as bs
+import undetected_chromedriver as uc
 from selenium.webdriver.firefox.options import Options
 
 def create_driver(headless=True):
-	print('create_driver()')
-	chrome_options = webdriver.ChromeOptions()
-	if headless:
-		chrome_options.add_argument("--headless")
-	chrome_options.add_argument("--log-level=3")
-	chrome_options.add_argument("--start-maximized")
-	chrome_options.add_argument('--no-sandbox')
-	chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36')
-	chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+	options = uc.ChromeOptions()
+	options.headless=headless
+	options.add_argument("--window-size=1920,1080")
+	options.add_argument('--ignore-certificate-errors')
+	options.add_argument('--allow-running-insecure-content')
+	options.add_argument('--headless')
 
-	chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-	chrome_options.add_experimental_option('useAutomationExtension', False)
-	chrome_options.add_argument("--disable-blink-features")
-	chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36")
-	chrome_options.add_experimental_option("prefs", { 
-	"profile.default_content_setting_values.media_stream_mic": 1, 
-	"profile.default_content_setting_values.media_stream_camera": 1,
-	"profile.default_content_setting_values.geolocation": 1, 
-	"profile.default_content_setting_values.notifications": 1,
-	"profile.default_content_settings.geolocation": 1,
-	"profile.default_content_settings.popups": 0
-  })
+	driver = uc.Chrome(use_subprocess=True, options=options)
 	
-	caps = DesiredCapabilities().CHROME
-
-	caps["pageLoadStrategy"] = "none"
-	
-	driver = webdriver.Chrome(desired_capabilities=caps,chrome_options=chrome_options)	
-
-	driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
- "source": """
-	  const newProto = navigator.__proto__
-	  delete newProto.webdriver
-	  navigator.__proto__ = newProto		
-	  """
-})
-	driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
-	driver.implicitly_wait(30)
-
-	#params = {
-	#"latitude": 55.5815245,
-	#"longitude": 36.825144,
-	#"accuracy": 100
-	#}
-	#driver.execute_cdp_cmd("Emulation.setGeolocationOverride", params)
-	#driver.refresh()
+	driver.set_page_load_timeout(15)
 	
 	return driver
-
 
 def get_page(url,driver):
 	driver.get(url)
